7034069f06c3ba618d512d9e8d5ae81328ef3c8f
yooodleee <accia25@naver.com>
2025-07-24 16:05:23 +0900

    [BP-6] DB 접근 및 HTTP 요청 처리

--- b2c-platform/apps/backend/src/user/user.controller.ts
+++ b2c-platform/apps/backend/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Post() 
+    create(@Body() body: any) {
+        console.log('POST /user 요청 받음:', body);
+        return this.userService.create(body);
+    }
+
+    @Get()
+    getAll() {
+        return this.userService.findAll();
+    }
+}
--- b2c-platform/apps/backend/src/user/user.service.ts
+++ b2c-platform/apps/backend/src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+
+    async findAll() {
+        return this.prisma.user.findMany();
+    }
+
+    async create(data: any) {
+        console.log('UserService.create() 호출됨:', data);
+        return { message: 'User created', user: data };
+    }
+}
